2e52db1a30a09b5086abb83b7bc0368618607709
Talha-Ahsan5 <yousuftalha005@gmail.com>
2025-10-06 04:03:07 -0700

    AppBar added

---
+++ meals/lib/something.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Something extends StatefulWidget {
+  const Something({super.key});
+
+  @override
+  State<Something> createState() => _SomethingState();
+}
+
+class _SomethingState extends State<Something> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My app'),
+      ),
+    );
+  }
+}
